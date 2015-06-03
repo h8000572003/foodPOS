dfc16e714c0e0e70767e84beb516e5f55f5694d4
andy <wa.addi@gmail.com>
2015-06-03 18:36:02 +0800

    DAO 環境基礎

---
+++ app/src/main/java/com/food/foodpos/MyApplication.java
@@ -0,0 +1,20 @@
+package com.food.foodpos;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+/**
+ * Created by 1109001 on 2015/6/3.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "22IjQiE5OTa6otvH93T8zZijDuB2Mdq9xYudN9zl", "GjlRYV1HZnnO0eE8uOAVsy0OaRssRoqZRmY7CoRz");
+
+    }
+}
