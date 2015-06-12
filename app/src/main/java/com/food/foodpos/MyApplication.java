a901eeb6d0080b828098438ad17b24dda62a88c5
andy <wa.addi@gmail.com>
2015-06-12 13:48:20 +0800

    test

--- app/src/main/java/com/food/foodpos/MyApplication.java
+++ app/src/main/java/com/food/foodpos/MyApplication.java
@@ -2,6 +2,8 @@ package com.food.foodpos;
 
 import android.app.Application;
 
+import com.food.foodpos.util.GcmRegistrationAsyncTask;
+
 /**
  * Created by 1109001 on 2015/6/3.
  */
--- app/src/main/java/com/food/foodpos/util/GcmRegistrationAsyncTask.java
+++ app/src/main/java/com/food/foodpos/util/GcmRegistrationAsyncTask.java
@@ -11,7 +11,13 @@ import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,7 +25,7 @@ import java.util.logging.Logger;
  * Created by 1109001 on 2015/6/10.
  */
 public class GcmRegistrationAsyncTask extends AsyncTask<Void, Void, String> {
-    private static Registration regService = null;
+
     private GoogleCloudMessaging gcm;
     private Context context;
 
@@ -32,23 +38,7 @@ public class GcmRegistrationAsyncTask extends AsyncTask<Void, Void, String> {
 
     @Override
     protected String doInBackground(Void... params) {
-        if (regService == null) {
-            Registration.Builder builder = new Registration.Builder(AndroidHttp.newCompatibleTransport(),
-                    new AndroidJsonFactory(), null)
-                    // Need setRootUrl and setGoogleClientRequestInitializer only for local testing,
-                    // otherwise they can be skipped
 
-                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                        @Override
-                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest)
-                                throws IOException {
-                            abstractGoogleClientRequest.setDisableGZipContent(true);
-                        }
-                    });
-            // end of optional local run code
-
-            regService = builder.build();
-        }
 
         String msg = "";
         try {
@@ -57,18 +47,41 @@ public class GcmRegistrationAsyncTask extends AsyncTask<Void, Void, String> {
             }
             String regId = gcm.register(SENDER_ID);
             msg = "Device registered, registration ID=" + regId;
-
-            // You should send the registration ID to your server over HTTP,
-            // so it can use GCM/HTTP or CCS to send messages to your app.
-            // The request to your server should be authenticated if your app
-            // is using accounts.
-            regService.register(regId).execute();
+            this.doTranslate(regId);
 
         } catch (Exception ex) {
             ex.printStackTrace();
             msg = "Error: " + ex.getMessage();
         }
         return msg;
+    }
+
+    private void doTranslate(String id) {
+        HttpURLConnection conn = null;
+        try {
+            if (Thread.interrupted()) {
+                throw new InterruptedException();
+            }
+
+
+            // 建立連線
+            URL url = new URL(
+                    "http://192.168.1.59:8080/SpringMVCV1/rest/gcm/insert?id="
+                            + id);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            ;
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
     }
 
     @Override
